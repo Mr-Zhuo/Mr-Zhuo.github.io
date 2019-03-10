32a4b8a5687c9a9bce7909f0910d9d66deb48f11
Mr-Zhuo <loselovezsl@outlook.com>
2019-03-10 20:33:09 +0800

    Update index-script.js

--- js/index-script.js
+++ js/index-script.js
@@ -2,7 +2,7 @@ let html = document.querySelector("html");
 let n=0;
 function funTest(){
     console.log("你已经点我",n,"次了");
-    if(n == 10)
+    if(n >= 10)
     {
         alert("我生气了,再见~~")
         window.close();
